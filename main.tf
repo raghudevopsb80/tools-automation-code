63eb5927e0868c7e4d100fe9bd8c573ed882b926
SVR Kalluru <raghuk.vit@gmail.com>
2024-07-19 07:14:00 +0530

    Terraform code

---
+++ main.tf
@@ -0,0 +1,6 @@
+module "tool-infra-create" {
+  for_each      = var.tools
+  source        = "./infra-create"
+  name          = each.key
+  instance_type = each.value["instance_type"]
+}
---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "tools" {
+  default = {
+
+    github-runner = {
+      instance_type = "t3.small"
+    }
+
+
+
+  }
+}
+
