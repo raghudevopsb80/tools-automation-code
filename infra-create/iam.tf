b83ecc1d78932ef0d726d1d14ed5dc1a28185da1
SVR Kalluru <raghuk.vit@gmail.com>
2024-07-22 06:36:08 +0530

    Adding Role in Terraform

--- infra-create/iam.tf
+++ infra-create/iam.tf
@@ -19,6 +19,11 @@ resource "aws_iam_role" "role" {
   }
 }
 
+resource "aws_iam_instance_profile" "instance-profile" {
+  name = "${var.name}-role"
+  role = aws_iam_role.role.name
+}
+
 resource "aws_iam_role_policy_attachment" "policy-attach" {
   count      = length(var.policy_name)
   role       = aws_iam_role.role.name
