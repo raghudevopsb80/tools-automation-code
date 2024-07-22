2eb027b1e3ff7ad61ecda6d10e490c795f1c071c
SVR Kalluru <raghuk.vit@gmail.com>
2024-07-22 07:20:10 +0530

    Adding DNS records for the servers

---
+++ ansible-run.sh
@@ -0,0 +1,2 @@
+ansible-playbook -i $1, tool-setup.yml -e ansible_user=ec2-user -e ansible_password=DevOps321 -e tool_name=github-ru
+nner
--- variables.tf
+++ variables.tf
@@ -17,3 +17,6 @@ variable "tools" {
   }
 }
 
+variable "hosted_zone_id" {
+  default = "Z00376861T6KFA01SJSIS"
+}
