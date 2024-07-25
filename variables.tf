18bb0c86c58ef2093f690c569bbe26d0e7aeb198
SVR Kalluru <raghuk.vit@gmail.com>
2024-07-25 08:06:15 +0530

    Clean old tools

--- variables.tf
+++ variables.tf
@@ -1,23 +1,6 @@
 variable "tools" {
   default = {
 
-    github-runner = {
-      instance_type = "t3.small"
-      policy_name = [
-        "AdministratorAccess"
-      ]
-      ports = {}
-    }
-
-    vault = {
-      instance_type = "t3.small"
-      policy_name   = []
-      ports = {
-        vault = 8200
-      }
-    }
-
-
   }
 }
 
