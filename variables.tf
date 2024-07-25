3a74dad06deb6e6e2c136904d4c18605cd576d2b
SVR Kalluru <raghuk.vit@gmail.com>
2024-07-25 08:10:11 +0530

    Wrong commit revert

--- variables.tf
+++ variables.tf
@@ -1,6 +1,23 @@
 variable "tools" {
   default = {
 
+    github-runner = {
+      instance_type = "t3.small"
+      policy_name = [
+        "AdministratorAccess"
+      ]
+      ports = {}
+    }
+
+    vault = {
+      instance_type = "t3.small"
+      policy_name   = []
+      ports = {
+        vault = 8200
+      }
+    }
+
+
   }
 }
 
