330d8659f75df9fd1e02232d7ad8a9cc71c01fbe
Raghu K <raghuk.vit@gmail.com>
2024-09-09 06:19:52 +0530

    RoboShop Kubernetes

--- variables.tf
+++ variables.tf
@@ -19,6 +19,15 @@ variable "tools" {
       volume_size = 20
     }
 
+    vault = {
+      instance_type = "r7i.large"
+      policy_name   = []
+      ports = {
+        vault = 80
+      }
+      volume_size = 30
+    }
+
     #     minikube = {
     #       instance_type = "t3.medium"
     #       policy_name   = []
