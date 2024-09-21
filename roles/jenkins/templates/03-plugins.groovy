cf96ba4e27499dd09a5540ef5b94b3d68e4ad6c4
Raghu K <raghuk.vit@gmail.com>
2024-09-21 07:04:39 +0530

    RoboShop Kubernetes

--- roles/jenkins/templates/03-plugins.groovy
+++ roles/jenkins/templates/03-plugins.groovy
@@ -1,23 +1,29 @@
-def plugins = [
-    "workflow-multibranch"
+import jenkins.model.*
+import hudson.PluginWrapper
+
+def pluginsToInstall = [
+    'workflow-multibranch'
 ]
 
-def instance = Jenkins.getInstance()
-def pm = instance.getPluginManager()
-def uc = instance.getUpdateCenter()
+// Reference Jenkins Plugin Manager and Update Center
+def pluginManager = Jenkins.instance.pluginManager
+def updateCenter = Jenkins.instance.updateCenter
 
-plugins.each { pluginName ->
-  if (!pm.getPlugin(pluginName)) {
-    def plugin = uc.getPlugin(pluginName)
+// Loop through each plugin and install if not already installed
+pluginsToInstall.each { pluginName ->
+  // Check if the plugin is already installed
+  if (!pluginManager.getPlugin(pluginName)) {
+    println "Installing plugin: ${pluginName}"
+    def plugin = updateCenter.getPlugin(pluginName)
+
     if (plugin) {
-      println("Installing " + pluginName)
-      plugin.deploy()
+      // Deploy plugin (install it)
+      plugin.deploy(true).get() // 'get()' waits for the installation to complete
+      println "Successfully installed ${pluginName}."
     } else {
-      println("Plugin not found: " + pluginName)
+      println "Plugin ${pluginName} not found in the Update Center."
     }
   } else {
-    println("Plugin already installed: " + pluginName)
+    println "Plugin ${pluginName} is already installed."
   }
 }
-
-
